5dba3beb028243449135389efe952db80ee6240c
Yan Kasabutski <yan6050773@gmail.com>
2026-07-05 15:02:41 +0200

    Clean up: v3.1 is the site

--- downloads/ESP32-Monitor-Firmware/Arduino_IDE_Libraries/Sensirion_Gadget_BLE_Arduino_Lib/src/Sample.cpp
+++
@@ -1,9 +0,0 @@
-#include "Sample.h"
-
-void Sample::writeValue(uint16_t value, size_t position) {
-    _write16BitLittleEndian(value, position);
-}
-
-void Sample::setByte(uint8_t byte, size_t position) {
-    _data[position] = byte;
-}
--- downloads/ESP32-Monitor-Firmware/Arduino_IDE_Libraries/Sensirion_UPT_Core/src/Sensirion_UPT_Core.h
+++
@@ -1,9 +0,0 @@
-#include "Arduino.h"
-
-#ifndef SENSIRION_UPT_CORE_H
-#define SENSIRION_UPT_CORE_H
-
-#include "BLEProtocol.h"
-#include "Measurement.h"
-
-#endif /* SENSIRION_UPT_CORE_H */
